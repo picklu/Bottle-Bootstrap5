e52d7732d35d96347e6d4fed077c42d745cfdd5f
Subrata Sarker <subrata_sarker@yahoo.com>
2022-09-27 09:59:33 +0600

    Use bottlepy for serving the data to the localhost

---
+++ app.py
@@ -0,0 +1,22 @@
+import platform
+from bottle import error, route, run, template
+from data import get_data
+
+
+@route("/")
+@route("/hello/<name>")
+def hello(name="Stranger"):
+    data = get_data("combined_data.json")
+    return template("index", name=name, data=data)
+
+
+@error(404)
+def error404(error):
+    return "Nothing here, sorry!"
+
+
+if __name__ == "__main__":
+    if platform.system() == "Linux":
+        run(host="localhost", port=5000, reloader=True, debug=True)
+    elif platform.system() == "Windows":
+        run(host="localhost", port=8000, reloader=True, debug=True)
