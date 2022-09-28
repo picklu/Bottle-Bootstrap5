bfa7b4a1e37dac5d437364cd6991da5a405dbbe9
Subrata Sarker <subrata_sarker@yahoo.com>
2022-09-28 10:55:03 +0600

    Remove unnecessary libraries from the project

--- app.py
+++ app.py
@@ -3,19 +3,15 @@ from os import environ, path
 from bottle import error, route, run, template
 
 
-def get_data(fileName):
-    """Return data in json format"""
-    data = []
-    filePath = path.join(path.dirname(__file__), fileName)
-    with open(filePath, mode="r", encoding="utf8") as f:
-        data = json.loads(f.read())
-
-    return data
+data_file = "combined_data.json"
 
 
 @route("/")
 def index():
-    data = get_data("combined_data.json")
+    data = []
+    filePath = path.join(path.dirname(__file__), data_file)
+    with open(filePath, mode="r", encoding="utf8") as f:
+        data = json.loads(f.read())
     return template("index", data=data)
 
 
@@ -25,7 +21,7 @@ def error404(error):
 
 
 if __name__ == "__main__":
-    if os.environ.get('APP_LOCATION') == 'heroku':
-        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    if environ.get('APP_LOCATION') == 'heroku':
+        run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
     else:
         run(host='localhost', port=8000, reloader=True, debug=True)
